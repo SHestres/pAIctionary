bd46ae924aada52c44691e4fb9fb72ce44a672a6
SHestres <sidneyhestres@gmail.com>
2024-04-14 21:53:32 -0500

    Comment the demo

--- client/index.js
+++ client/index.js
@@ -9,20 +9,27 @@ const path = require('path');
 
 const port = 3000;
 
+// Serve js files
 app.use(express.static(path.join(__dirname, 'src')));
 
+// Serve basic html with express
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+// Sockets setup
 io.on('connection', (socket) => {
     console.log('a user connnected');
 
+    // When recieving message from any connection...
     socket.on('message', (message) => {
         console.log(message);
+        
+        // Relay that message to all connections with io.emit()
         io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
     });
 });
 
+// Can't use app.listen, it will create a new httpserver
 httpServer.listen(port);
 console.log('listening at http://localhost:' + port)
--- client/src/app.js
+++ client/src/app.js
@@ -2,13 +2,16 @@ console.log("js loaded");
 
 const socket = io();
 
+// Post any recieved messages into the message list
 socket.on('message', text => {
     const el = document.createElement('li');
     el.innerHTML = text;
     document.querySelector('ul').appendChild(el);
 });
 
+// Send message to the server when button is clicked
 document.querySelector('button').onclick = () => {
     const text = document.querySelector('input').value;
+    // Send message to the server
     socket.emit('message', text)
 }
