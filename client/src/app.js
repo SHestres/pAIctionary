dbc825c225fe0681b4d0fcd399dccd8f7c16ebda
SHestres <sidneyhestres@gmail.com>
2024-05-03 00:15:28 -0500

    Create pre-turn messages for players

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,20 @@
 var readyToPrompt = true;
 var lastPrompt = "";
 var waitingPrompt = "";
+var infoText = document.querySelector('.infoText');
+var promptInput = document.querySelector('.promptInput');
+
+const defaultMsg = "Wait for the game to Start!";
+const drawerMsg = "You are Drawing! \nAre you ready?";
+const guesserMsg = "Your team is drawing this round! Get ready to guess!";
+const waiterMsg = "Your team isn't up this round! Dont' give away your thoughts to your opponents!";
+
+infoText.innerHTML = defaultMsg;
+promptInput.classList.add('hide');
+
+promptInput.oninput = (e) => {
+    promptServer(e.target.value)
+}
 
 function promptServer(prompt){
     waitingPrompt = prompt;
@@ -12,16 +26,13 @@ function promptServer(prompt){
 }
 
 // Send message to the server when button is clicked
-document.querySelector('button').onclick = () => {
+/*document.querySelector('button').onclick = () => {
     const text = document.querySelector('input').value;
     // Send message to the server
     promptServer(text)
 }
+*/
 
-
-document.querySelector('input').oninput = (e) => {
-    promptServer(e.target.value)
-}
 
 
 // Post any recieved messages into the message list
@@ -41,14 +52,49 @@ socket.on('image', image => {
     socket.emit('finishedImage')
 });
 
+const playerStates = {
+    NONE: "NONE",
+    DRAW: "DRAW",
+    GUESS: "GUESS",
+    WAIT: "WAIT"
+}
+var playerState = ""
+
+socket.emit('getPlayerStates', state => {
+    switch (state){
+        case playerStates.NONE:
+            infoText.innerHTML = defaultMsg;
+            break;
+        case playerStates.DRAW:
+            infoText.innerHTML = drawerMsg;
+            break;
+        case playerStates.GUESS:
+            infoText.innerHTML = guesserMsg;
+            break;
+        case playerStates.WAIT:
+            infoText.innerHTML = waiterMsg;
+            break;
+        default:
+            console.error('Invalid player state sent by server')
+            break;
+    }
+})
+
 socket.on('youDraw', () => {
+    playerState = playerStates.DRAW;
+    infoText.innerHTML = drawerMsg;
+    //TODO: Add Ready! button
     console.log("You're drawing");
 })
 
 socket.on('youGuess', () => {
+    playerState = playerStates.GUESS;
+    infoText.innerHTML = guesserMsg;
     console.log("You're guessing");
 });
 
 socket.on('youWait', () => {
+    playerState = playerStates.WAIT;
+    infoText.innerHTML = waiterMsg;
     console.log("You're waiting")
 })
